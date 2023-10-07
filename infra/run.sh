f845cfd853c2ad738636200700f58f665f9a53cf
Alexander Frick <Alex313031@gmail.com>
2023-10-07 02:16:23 -0500

    fix run.sh

--- infra/run.sh
+++ infra/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Copyright (c) 2023 Alex313031 and Midzer.
+# Copyright (c) 2023 Alex313031
 
-./chrome --user-data-dir=${PWD}/user_data --ppapi-flash-path=${PWD}/PepperFlash/_platform_specific/linux_x64/libpepflashplayer.so --ppapi-flash-version=34.0.0.137 --no-sandbox
+./chrome "--user-data-dir=${PWD}/user_data" "--ppapi-flash-path=${PWD}/PepperFlash/_platform_specific/linux_x64/libpepflashplayer.so" "--ppapi-flash-version=34.0.0.137" "--no-sandbox" "$@"
